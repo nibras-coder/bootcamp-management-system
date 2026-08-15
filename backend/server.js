b2fa852560f270d79375976ceddaee350350aa6e
Ruba Kedir <rubakedir@gmail.com>
2026-08-15 09:48:10 -0700

    Implement backend authentication

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+connectDB();
+
+app.use("/api/auth", authRoutes);
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Bootcamp Management System API is running",
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
