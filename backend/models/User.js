eb6720f59130b92bb5b8f7b96a118e4e3fd390c1
Hayat Abdulfetah <hayatabdulfetah7@gmail.com>
2026-08-16 08:54:48 +0300

    merging

--- backend/models/User.js
+++ backend/models/User.js
@@ -43,13 +43,15 @@ const userSchema = new mongoose.Schema(
 );
 
 // Hash password before saving
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
-
+userSchema.pre("save", async function () {
+  // If the password wasn't changed, skip this whole function
+  if (!this.isModified("password")) {
+    return;
+  }
+  
+  // Scramble the password
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
-
-  next();
 });
 
 // Compare password during login
